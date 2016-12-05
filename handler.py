3e244e86c674f60747adc50addfff5f1ede08c7f
Beto Esquivel <ppbeto94@gmail.com>
2016-12-05 11:55:49 +0000

    Comment on the setup code for each concurrent thread in the AWS Lambda

--- handler.py
+++ handler.py
@@ -2,6 +2,9 @@
 from textblob_setup import setup
 from pydata29 import processor, io_handler
 
+# Download language model into AWS Lambda container.
+# This is only done once per concurrent thread
+# because the container is reused on subsequent calls.
 setup()
 
 def process(event, context):
@@ -10,9 +13,3 @@ def process(event, context):
     return {
         "filepath": filepath,
     }
-
-if False:
-
-    from handler import *
-
-    # process(None, None)
